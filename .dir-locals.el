ac99f3fdd3d92becbccfd5b4231b572432e4d099
Martin Bodin <mbodin@dim.uchile.cl>
2017-07-19 11:09:22 -0400

    Repository reorganisation.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,8 @@
 ((coq-mode . ((coq-load-path . (
         ("." "Main")
+        ("low" "Low")
+        ("structured" "Structured")
+        ("high" "High")
         ("lib" "Lib")
     	("lib/tlc/src" "TLC")
     )))))
