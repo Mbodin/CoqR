26ee90e92986bf3094753ddc5b984f16484bc338
Martin Bodin <martin.bodin@ens-lyon.org>
2018-02-08 12:49:38 -0300

    Making the grammar terminate in practise.

--- gen/gen.ml
+++ gen/gen.ml
@@ -12,6 +12,10 @@ let file =
     else open_in Sys.argv.(1)
   | _ -> failwith "Too many arguments!"
 
+let debug = false
+let full_debug = false
+let max_step = 1000
+
 let rules =
   let buf = Lexing.from_channel file in
   Parser.main Lexer.lex buf
@@ -40,6 +44,8 @@ let convert =
 
 let rules : rule_automaton = convert rules
 
+let stats = ref []
+
 let step p_often p_rare l =
   let rec merge_local = function
     | ([] | [_]) as l -> l
@@ -50,23 +56,57 @@ let step p_often p_rare l =
     | Str str :: l -> Str str :: map_first_id f l
     | Id id :: l -> f id @ l in
   let transition id =
+    let rec updatestat = function
+        | [] -> [(id, 1)]
+        | (id', c) :: l ->
+          if id = id' then (id, 1 + c) :: l
+          else (id', c) :: updatestat l in
+    if debug then stats := updatestat !stats ;
     let (often, normal, rare) =
       try List.assoc id rules
       with Not_found -> failwith ("Unkown identifier: " ^ id) in
     let choose l =
       List.nth l (Random.int (List.length l)) in
-    if Random.float 1.0 < p_often then choose often
-    else if Random.float 1.0 < p_rare then choose (often @ normal)
-    else choose (often @ normal @ rare) in
+    if Random.float 1.0 < p_often
+       && often <> [] then choose often
+    else if Random.float 1.0 < p_rare
+         && (often <> [] || normal <> []) then choose (often @ normal)
+    else (
+        assert (often <> [] || normal <> [] || rare <> []) ;
+        choose (often @ normal @ rare)
+      ) in
   merge_local (map_first_id transition l)
 
+let print_step l =
+  let print = function
+    | Str str -> "\"" ^ String.escaped str ^ "\""
+    | Id id -> id in
+  print_endline (String.concat " " (List.map print l))
+
+let print_stats _ =
+  let print (id, c) = id ^ ": " ^ string_of_int c in
+  stats := List.sort (fun (_, c1) (_, c2) -> compare c2 c1) !stats ;
+  print_endline (String.concat ", " (List.map print !stats))
+
 let generate p_often p_rare =
-  let rec aux l =
-    match step p_often p_rare l with
-    | [Str str] -> str
-    | l -> aux (step p_often p_rare l) in
-  aux [Id start_identifier]
+  let emergency l =
+    let print = function
+      | Str str -> str
+      | Id id -> "#" ^ id ^ "\n" in
+    String.concat "" (List.map print l) in
+  let rec aux i l =
+    if max_step > 0 && i > max_step then
+      emergency l
+    else
+      match step p_often p_rare l with
+      | [] -> ""
+      | [Str str] -> str
+      | l ->
+        if full_debug then print_step l ;
+        if debug then print_stats () ;
+        aux (1 + i) (step p_often p_rare l) in
+  aux 0 [Id start_identifier]
 
 let _ =
-  print_endline (generate 0.3 0.5)
+  print_endline (generate 0.3 0.7)
 
--- tests/testExtra.R
+++ tests/testExtra.R
@@ -324,9 +324,9 @@ a <- 1L ; attr (a, "f") <- 2 ; a ; attr (a, "f") ; attr (a, "f") <- function (x)
 a <- 2i ; attr (a, "f") <- 2 ; a ; attr (a, "f") ; attr (a, "f") <- function (x) x ; a ; attr (a, "f") ; attr (a, "f") <- NULL ; a ; attr (a, "f") ; attr (a, "f") <- NA ; a ; attr (a, "f")
 a <- NaN ; attr (a, "f") <- 2 ; a ; attr (a, "f") ; attr (a, "f") <- function (x) x ; a ; attr (a, "f") ; attr (a, "f") <- NULL ; a ; attr (a, "f") ; attr (a, "f") <- NA ; a ; attr (a, "f")
 a <- NA ; attr (a, "f") <- 2 ; a ; attr (a, "f") ; attr (a, "f") <- function (x) x ; a ; attr (a, "f") ; attr (a, "f") <- NULL ; a ; attr (a, "f") ; attr (a, "f") <- NA ; a ; attr (a, "f")
-a <- NULL ; attr (a, "f") <- 2 ; a ; attr (a, "f") ; attr (a, "f") <- function (x) x ; a ; attr (a, "f") ; attr (a, "f") <- NULL ; a ; attr (a, "f") ; attr (a, "f") <- NA ; a ; attr (a, "f")
 a <- "" ; attr (a, "f") <- 2 ; a ; attr (a, "f") ; attr (a, "f") <- function (x) x ; a ; attr (a, "f") ; attr (a, "f") <- NULL ; a ; attr (a, "f") ; attr (a, "f") <- NA ; a ; attr (a, "f")
 a <- "" ; attr (a, "") <- 2 ; a ; attr (a, "") ; attr (a, "") <- function (x) x ; a ; attr (a, "") ; attr (a, "") <- NULL ; a ; attr (a, "") ; attr (a, "") <- NA ; a ; attr (a, "")
+a <- NULL ; attr (a, "f") <- 2
 a <- -10:10 ; a ; a [3] <- 9 ; a ; a [6:9] ; a > 0 ; a = 0 ; a [a = 0] <- 8 ; a ; a [a < 1] <- 7 ; a ; a [-2] ; a [-4:-7]
 a <- -10:10 ; attr (a, "x") <- 9 ; attr (a, "x") ; attr (a [1], "x") ; attr (a [-1], "x") ; attr (a [1], "y") <- 8 ; a ; attr (a [1], "y") ; attr (a, "y") ; b <- a [1] ; attr (b, "z") <- 7 ; attr (b, "z") ; attr (a [1], "z")
 a <- -10:10 ; a [c (TRUE, FALSE, TRUE)] ; a [80:90] ; a [-9:0] ; a [c ("x", "y")] ; a [NA] ; a [NaN] ; a [c (TRUE, FALSE, NA)] ; a ["TRUE"] ; a [0] ; a [a]
