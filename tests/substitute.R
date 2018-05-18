526bfe8da3e41b4d34fc88a62906f38d07ac9c61
Tomas <toumasaldia@gmail.com>
2018-05-17 20:38:38 -0400

    Update submodule compare

--- tests/substitute.R
+++ tests/substitute.R
@@ -10,6 +10,9 @@ a <- 3; substitute(a, list(a = a + 1))
 substitute(a, list())
 substitute(a, list(b = 3))
 b <- 1; substitute(a, list(a = b + 1))
+substitute(a + b, list(a = 2))
+substitute(function(x) { x + y }, list(x = function () {NULL}, y = environment())
+
 f <- function (x) x; b <- 3; substitute(a, list(a = f(b)))
 
 substitute(substitute())
@@ -20,9 +23,13 @@ substitute(1, environment()); substitute(1, environment())
 substitute(1); substitute(1)
 substitute(NULL); substitute(NULL)
 f <- function (x) substitute(x); f(NULL); f(NULL)
+f <- function (x) substitute(x, list(x=NA)); f(NULL); f(NULL)
 f <- function (x) substitute(x, environment()); f(NULL); f(NULL)
 e <- environment(); f <- function () substitute(x, e); f(); f()
+f <- function () { substitute (x); substitute(x) }; f(); f()
 
+f <- function (x) {a <- x(); substitute(a)}; g <- function () {1}; f(g) ; f(g)
+g <- function (x) {substitute(a + 1, list(a = x))}; f <- function (x) {x(3)}; f(g); f(g)
 
 typeof <- function (x) .Internal (typeof (x)); typeof(substitute(substitute()))
 typeof <- function (x) .Internal (typeof (x)); typeof(substitute(function () substitute(x), list(x=2)))
