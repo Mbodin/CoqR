924b12ac4dc338a2b6c4b56bd3f39c94f3028f79
Tomas <toumasaldia@gmail.com>
2018-06-16 00:28:10 -0400

    Add body to base library for FastR

--- Rlib/fastr_base/formals.R
+++ Rlib/fastr_base/formals.R
@@ -22,6 +22,22 @@ formals <- function(fun = sys.function(sys.parent())) {
     .Internal(formals(fun))
 }
 
+body <- function(fun = sys.function(sys.parent())) {
+    if(is.character(fun))
+	fun <- get(fun, mode = "function", envir = parent.frame())
+    .Internal(body(fun))
+}
+
+
+`body<-` <- function (fun, envir = environment(fun), value) {
+    if(!is.function(fun)) warning("'fun' is not a function") # TODO[2017]: stop()
+    if (is.expression(value)) {
+        if (length(value) > 1L)
+            warning("using the first element of 'value' of type \"expression\"")
+        value <- value[[1L]]
+    }
+    as.function(c(as.list(formals(fun)), list(value)), envir)
+}
 
 `formals<-` <- function (fun, envir = environment(fun), value)
 {
