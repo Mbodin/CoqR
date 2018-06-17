dcd3c2425f776f7797fe2d9246aa8885301e156f
Tomas <toumasaldia@gmail.com>
2018-06-17 16:14:50 -0400

    Add realSubscript but fails with run%exit

--- src/core/CSubscript.v
+++ src/core/CSubscript.v
@@ -25,7 +25,7 @@ Require Import CRinternals.
 Require Import CRinlinedfuns.
 Require Import CAttrib.
 Require Import CDuplicate.
-
+Require Import CArithmetic.
 
 Section Parameterised.
 
@@ -42,6 +42,102 @@ Local Coercion Pos.to_nat : positive >-> nat.
 
 Local Coercion int_to_double : Z >-> double.
 
+
+Definition logicalSubscript (S : state) (s : SEXP) (ns nx : nat) (stretch : nat) (call : SEXP) : result SEXP :=
+  add%stack "logicalSubscript" in
+    result_not_implemented "logicalSubscript".
+
+Definition realSubscript S s (ns nx stretch : nat) call  :=
+  add%stack "realSubscript" in
+    let isna := false in
+    let canstretch := decide (stretch > 0) in
+    let stretch := 0%Z in
+    let min := (0 : double) in
+    let max := (0 : double) in
+    do%success (min, max, isna) := (min, max, isna)
+    for i from 0 to ns - 1 do                                
+        read%Real ii := s at i using S in
+        if R_FINITE ii then                                
+            let min := ifb ii < min then ii else min in
+            let max := ifb ii > max then ii else max in
+            result_success S (min, max, isna)
+        else                 
+            result_success S (min, max, true)
+    using S in
+
+    let%success stretch :=                                     
+    ifb max > nx then                                     
+        if canstretch then 
+            result_success S max
+        else
+            result_error S "subscript out of bounds"
+    else
+        result_success S (stretch : double)
+    using S in
+
+    run%exit 
+    ifb min < 0%Z then
+        ifb max = 0%Z /\ negb isna then
+            let stretch := 0%Z in
+            let%success indx := allocVector globals S LglSxp (Z.to_nat nx) using S in 
+            do%success for i from 0 to nx - 1 do write%Logical indx at i := 1 using S in result_skip S
+            using S in                                                                  
+            do%success
+            for i from 0 to ns - 1 do
+                read%Real dx := s at i using S in
+                ifb R_FINITE dx /\ dx <> 0 /\ -(Double.double_to_int_zero dx) <= nx%Z then                                                                 
+                    let ix := -((Double.double_to_int_zero dx) + 1) in
+                    write%Logical indx at Z.to_nat ix := 0 using S in result_skip S
+                else
+                    result_skip S
+            using S in
+
+            let%success s := logicalSubscript S indx nx nx (Z.to_nat stretch) call using S in
+            result_rreturn S (s : SEXP, stretch : int)
+        else
+            result_error S "only 0's may be mixed with negative subscripts"
+    else
+        (* Only return a REALSXP index if we need to *)
+        let cnt := 0 in
+        let int_ok := true in
+        do%success (int_ok, cnt) := (int_ok, cnt) 
+        for i from 0 to ns - 1 do
+            read%Real ds := s at i using S in
+            let int_ok := ifb R_FINITE ds /\ ds > INT_MAX then false else int_ok in
+            let cnt := ifb negb (R_FINITE ds) \/ ds%Z <> 0 then cnt + 1 else cnt in
+            result_success S (int_ok, cnt)
+        using S in
+
+        let%success indx :=
+        if int_ok then 
+            let%success indx := allocVector globals S IntSxp cnt using S in
+            do%success cnt := 0
+            for i from 0 to ns - 1 do
+                read%Real ds := s at i using S in
+                let ia := if negb (R_FINITE ds) then NA_INTEGER else Double.double_to_int_zero ds in
+                ifb ia <> 0 then
+                    write%Integer indx at cnt := ia using S in
+                    result_success S (cnt + 1)
+                else
+                    result_success S cnt
+            using S in
+            result_success S indx 
+        else
+            let%success indx := allocVector globals S RealSxp cnt using S in
+            do%success cnt := 0
+            for i from 0 to ns - 1 do
+                read%Real ds := s at i using S in                   
+                ifb negb (R_FINITE ds) \/ ds%Z <> 0 then                   
+                    write%Real indx at cnt := ds using S in
+                    result_success S (cnt + 1)
+                else
+                    result_success S cnt
+            using S in                       
+            result_success S indx 
+        using S in
+        result_rreturn S (indx : SEXP, Double.double_to_int_zero stretch : int) 
+    using S in
+    result_success S (R_NilValue : SEXP, Double.double_to_int_zero stretch : int).
 
 
 Definition makeSubscript S (x s call : SEXP)  :=
@@ -86,7 +182,7 @@ Definition makeSubscript S (x s call : SEXP)  :=
       result_success S (ans, 0)
     | LglSxp => unimplemented_function "logicalSubscript"  
     | IntSxp => unimplemented_function "integerSubscript"
-    | RealSxp => unimplemented_function "realSubscript"
+    | RealSxp => realSubscript S s ns nx stretch call
     | StrSxp =>
       let%success names := getAttrib globals runs S x R_NamesSymbol using S in
       unimplemented_function "stringSubscript"
