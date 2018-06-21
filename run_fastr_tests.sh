54141314fa38466e47e0839138a7e1d39c4726cb
Tomas <toumasaldia@gmail.com>
2018-06-21 14:39:29 -0400

    Add script to run FastR tests

---
+++ run_fastr_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+title='FastR $1 Tests'
+
+echo "Running tests for FastR - $1"
+
+make
+
+cp Rlib/bootstrapping.state Rlib/bootstrapping.initial.state
+
+echo "Including base library for $1 tests"
+
+for f in Rlib/fastr_oracle/$1/*; do
+    echo "Adding file $f to initial state"
+    cat $f | ./src/runR.native -initial-state Rlib/bootstrapping.state -final-state Rlib/bootstrapping.state -hide-prompt
+    echo "Finished including file to initial state"
+done
+
+echo "Finished including base library"
+
+
+echo "Activating virtualenv"
+source $COQR_TEST_PATH/venv/bin/activate
+
+echo "Running tests"
+./compare/run_all.py tests/FastR/builtins/$1 -s -t '$title' -d
+
+echo "Deactivating virtualenv"
+source $COQR_TEST_PATH/venv/bin/deactivate
